fa60d9d4f614d9d8faa16135e3daa6652edc48b0
lucky7817 <lucky78@abv.bg>
2023-05-30 10:37:35 +0300

    Extract routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
 const routes = require('./routes');
 
 
@@ -14,11 +12,7 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController);
-app.use('/cubes', cubeController);
-app.get('*', (req, res) => {
-    res.redirect('/404');
-})
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
 
