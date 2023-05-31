ee419602fc4411fad5a0f9e5e99f802b90c38c00
lucky7817 <lucky78@abv.bg>
2023-05-31 11:11:35 +0300

    Add create accessory page

---
+++ src/controllers/accessoryController.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+router.get('/create', (req, res) => {
+    res.render('accessory/create');
+});
+
+router.post('/create', (req, res) => {
+    const body = req.body
+    console.log(body);
+
+    res.redirect('/')
+})
+
+module.exports = router;
