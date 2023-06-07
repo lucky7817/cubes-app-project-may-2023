4bf4b39a8c80384d38abc556502572f489fb3808
lucky7817 <lucky78@abv.bg>
2023-06-07 09:37:18 +0300

    Add delete cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -5,7 +5,7 @@ const accessoryManager = require('../managers/accessoryManager');
 
 // Path /cubes/create
 router.get('/create', (req, res) => {
-    res.render('create');
+    res.render('cube/create');
 });
 
 router.post('/create', async (req, res) => {
@@ -34,7 +34,7 @@ router.get('/:cubeId/details', async (req, res) => {
         return res.redirect('/404');
     }
 
-    res.render('details', { cube });
+    res.render('cube/details', { cube });
 });
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
@@ -54,8 +54,13 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
     await cubeManager.attachAccessory(cubeId, accessoryId);
 
     res.redirect(`/cubes/${cubeId}/details`);
+});
 
-})
+router.get('/:cubeId/delete', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+
+    res.render('cube/delete', { cube });
+});
 
 
 module.exports = router;
