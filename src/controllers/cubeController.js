b04fadcf53a10aaf720ac5669f3d56e9227e07d5
lucky7817 <lucky78@abv.bg>
2023-06-07 11:01:40 +0300

    Generate options for difficulty level

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const cubeManager = require('../managers/cubeManager');
 const accessoryManager = require('../managers/accessoryManager');
+const { getDifficultyOptionsViewData } = require('../utils/viewHelpers');
 
 // Path /cubes/create
 router.get('/create', (req, res) => {
@@ -29,7 +30,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/:cubeId/details', async (req, res) => {
     const cube = await cubeManager.getOneWithAccessories(req.params.cubeId).lean();
-    
+
     if (!cube) {
         return res.redirect('/404');
     }
@@ -59,7 +60,9 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
 router.get('/:cubeId/delete', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
-    res.render('cube/delete', { cube });
+    const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+
+    res.render('cube/delete', { cube, options });
 });
 
 router.post('/:cubeId/delete', async (req, res) => {
@@ -68,10 +71,14 @@ router.post('/:cubeId/delete', async (req, res) => {
     res.redirect('/');
 });
 
+
+
 router.get('/:cubeId/edit', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
 
-    res.render('cube/edit', { cube });
+    const options = getDifficultyOptionsViewData(cube.difficultyLevel);
+
+    res.render('cube/edit', { cube, options });
 });
 
 router.post('/:cubeId/edit', async (req, res) => {
